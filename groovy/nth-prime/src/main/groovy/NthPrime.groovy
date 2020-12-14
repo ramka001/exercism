a9c24edffedbd3d7304246faa7917112bbb75a5d
Admin <rkumar@adaptavist.com>
2020-12-14 13:15:24 +0800

    NthPrime updated

--- groovy/nth-prime/src/main/groovy/NthPrime.groovy
+++ groovy/nth-prime/src/main/groovy/NthPrime.groovy
@@ -1,7 +1,43 @@
 class NthPrime {
 
     static nth(int n) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+
+        if(n==0) {
+            throw new ArithmeticException()
+        }
+
+        def maxCheck
+        def isPrime
+        def primeList = []
+
+        if(n >= 1 && n < 10) {
+            maxCheck = n + 25
+        } else if(n>=10 && n <20) {
+            maxCheck = n + 50
+        } else if(n >= 100 && n < 10000) {
+            maxCheck = n + 1000
+        } else if(n >= 10000 ) {
+            maxCheck = n + 100000
+        }
+
+
+        for (int i = 2; i <= maxCheck; i++) {
+            isPrime = checkPrime(i)
+            if(isPrime) {
+                primeList.add(i)
+            }
+        }
+
+        return primeList[n-1]
+
     }
 
+    static boolean checkPrime(int numberToCheck) {
+        for (int i = 2; i <= numberToCheck / 2; i++) {
+                if(numberToCheck % i == 0) {
+                    return false
+                }
+        }
+        return true
+    }
 }
